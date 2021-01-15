208ef0a0366efeb9b196b54ced24f8c8d6b19480
ZeKai Chong <yokai.czk@gmail.com>
2021-01-15 16:23:30 +1030

    296 ms

---
+++ solutions/1642. Furthest Building You Can Reach.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int size = heights.size()-1;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i = 0; i < size; i++){
+            if(heights[i] < heights[i+1]){
+                int distance = heights[i+1] - heights[i];
+                pq.push(distance);
+            }
+            
+            if(pq.size() > ladders){
+                bricks -= pq.top();
+                pq.pop();
+                
+                if(bricks < 0){
+                    return i;
+                }
+            }
+        }
+        
+        return size;
+    }
+};
