3ac4f443ff3e3b2f84f551615a0852bf29f1451a
ZeKai Chong <yokai.czk@gmail.com>
2021-01-15 16:24:08 +1030

    8 ms

--- solutions/1470. Shuffle the Array.cpp
+++ solutions/1470. Shuffle the Array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> arr;
-        for(int i = 0; i < n; i++){
-            arr.push_back(nums[i]);
-            arr.push_back(nums[i+n]);
+        vector<int> result;
+        for(int i = 0, j = n; i < n; i++, j++){
+            result.push_back(nums[i]);
+            result.push_back(nums[j]);
         }
-        return arr;
+        
+        return result;
     }
 };
