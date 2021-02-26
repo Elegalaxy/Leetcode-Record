712d3f745a14ad10e61645f559cc72a4cf25aef1
ZeKai Chong <yokai.czk@gmail.com>
2021-02-26 14:58:45 +1030

    60 ms

--- solutions/187. Repeated DNA Sequences.cpp
+++ solutions/187. Repeated DNA Sequences.cpp
@@ -4,7 +4,7 @@ public:
         vector<string> res;
         if(s.length() < 10) return res;
         
-        map<string, int> m;
+        unordered_map<string, int> m;
         string temp;
         for(int i = 0; i < s.length()-9; i++){
             temp = s.substr(i, 10);
