ee2d4ed16b605dd1bcf9833ceeedfc19650aa603
ZeKai Chong <yokai.czk@gmail.com>
2021-11-22 15:34:39 +1030

    12 ms

---
+++ solutions/242. Valid Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> mp1, mp2;
+        
+        for(char i:s)
+            mp1[i]++;
+        for(char i:t)
+            mp2[i]++;
+        
+        return (mp1==mp2);
+    }
+};
