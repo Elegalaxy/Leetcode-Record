3dbaf99969eb0668e81e51b7573d979a2650e0b5
ZeKai Chong <yokai.czk@gmail.com>
2021-11-27 12:30:06 +1030

    12 ms

--- solutions/242. Valid Anagram.cpp
+++ solutions/242. Valid Anagram.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mp1, mp2;
         
-        for(char i:s)
-            mp1[i]++;
-        for(char i:t)
-            mp2[i]++;
-        
-        return (mp1==mp2);
     }
 };
