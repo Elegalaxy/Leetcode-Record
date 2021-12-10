4c8aacbc62f113d7151b1b6fc04ebc6cae56dbd8
ZeKai Chong <yokai.czk@gmail.com>
2021-12-10 15:43:17 +1030

    0 ms

--- solutions/2073. Time Needed to Buy Tickets.cpp
+++ solutions/2073. Time Needed to Buy Tickets.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int res = 0;
-        while(tickets[k] != 0){
-            for(int i = 0; i <tickets.size(); i++){
-                if(tickets[i] != 0){
-                    res++;
-                    tickets[i]--;
-                    if(tickets[k] == 0)
-                        break;
-                }
-            }
-        }
+        int t = tickets[k];
+        
+        for(int i = 0; i <tickets.size(); i++){
+            res+=min(t-(i > k), tickets[i]);
+        }
+        
 ​
         return res;
 ​
